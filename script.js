f47457b126a521c60c8c754acdcd86c84483607c
Azhaghiri <ganadhan6528@gmail.com>
2024-10-22 12:51:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
     //Write your code here
+ event.preventDefault();
 
+    // Access the form element using document.forms
+    let form = document.forms["form1"];
+
+    // Get the values of the input fields
+    let firstName = form["fname"].value;
+    let lastName = form["lname"].value;
+
+    // Display the combined first and last name in an alert
+    alert(firstName + " " + lastName);
 }
